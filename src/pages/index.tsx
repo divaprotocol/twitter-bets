0ccc502737210d9e7643342171182bfb5f666147
Tarun Yadav <tarunyadav9761@gmail.com>
2022-11-22 11:37:13 +0530

    init setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+function HomePage() {
+	return (
+		<div>
+			<Typography variant="h1" fontFamily={'PPCirka'}>
+				Twitter Bets
+			</Typography>
+		</div>
+	)
+}
+
+export default HomePage
